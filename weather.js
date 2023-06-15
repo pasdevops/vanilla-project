14a55b16d5f28e084be9dc7f7b173b2d7962572d
Pas <askpeevee@gmail.com>
2023-06-15 22:47:35 +0100

    Weather Tasks

--- weather.js
+++ weather.js
@@ -7,64 +7,55 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon"); 
 
-
-// Map over an array to generate HTML
-// const numbers = [1, 2, 3, 4, 5]; 
-// const example = document.querySelector(".example");
-// const newHtml = numbers.map((number) => {
-//   return `<li>${number}</li>`
-// });
-
-// example.innerHTML = newHtml.join("");
-
-
-// Creating a function using AXIOS to make API third party website. 
+// Creating a function using AXIOS to make API call to a third party website. 
 async function checkWeather(city) {
-  const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=e945bb4b32f31db4a881497123459508`
-  );
-  
+  // using try catch to log error on the console 
+  try {
+    const cord = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=e945bb4b32f31db4a881497123459508`
+    );
+    console.log(cord.data[0].lat);
+    console.log(cord.data[0].lon);
 
-    
-    // Error message for invalid city search
-  if (response.status == 404) {
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".Weather").style.display = "none";
+    const lon = cord.data[0].lon;
+    const lat = cord.data[0].lat;
 
-  } else {
- 
+
+
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=e945bb4b32f31db4a881497123459508`
+    );
+
     const { main, weather, wind, name, country } = response.data;
-     
 
-        // Manipulating HTML USING DOM on the required objects
-        document.querySelector(".city").innerHTML = name;
-        document.querySelector(".temp").innerHTML = Math.round(main.temp) + "°C";
-        document.querySelector(".humidity").innerHTML = main.humidity + "%";
-        document.querySelector(".wind").innerHTML = wind.speed + "km/h";
-        document.querySelector(".pressure").innerHTML = main.pressure + "CI";
-       
-       
-       
-        
-    
+    // Manipulating HTML USING DOM on the required objects
+    document.querySelector(".city").innerHTML = name;
+    document.querySelector(".temp").innerHTML = Math.round(main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = main.humidity + "%";
+    document.querySelector(".wind").innerHTML = wind.speed + "km/h";
+    document.querySelector(".pressure").innerHTML = main.pressure + "CI";
 
-        // Update weather accoding to condition using conditional statement
-        if (weather[0].main == "Clouds") {
-          weatherIcon.src = "images/clouds.png";
-        } else if (weather[0].main == "Clear") {
-          weatherIcon.src = "images/clear.png";
-        } else if (weather[0].main == "Rain") {
-          weatherIcon.src = "images/clear.png";
-        } else if (weather[0].main == "Drizzle") {
-          weatherIcon.src = "images/clear.png";
-        } else if (weather[0].main == "Mist") {
-          weatherIcon.src = "images/mist.png";
-        }
-
-        document.querySelector(".weather").style.display = "block"; 
-        document.querySelector(".error").style.display = "none";
+    // Update weather accoding to condition using conditional statement
+    if (weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (weather[0].main == "Rain") {
+      weatherIcon.src = "images/clear.png";
+    } else if (weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/clear.png";
+    } else if (weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
     }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  } catch(err) {
+    document.querySelector(".error").style.display = "block";
+    console.log("An error occured", err);
 }
+}
+
 // Adding an event listerner to the function called
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
